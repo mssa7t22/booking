6049ddc950f50bc16460e62b27ad139e761dcd9c
mssa7t22 <105381802+mssa7t22@users.noreply.github.com>
2023-01-03 13:49:30 +0400

    Add files via upload

---
+++ js/swiper.js.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+  delay: 3500,
+  disableOnInteraction: false,
+},
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
